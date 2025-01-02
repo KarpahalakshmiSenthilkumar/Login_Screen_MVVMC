14f174189f8ae034cb8bc56e733fc929d3666b74
Karpahalakshmi Senthilkumar <sklsuja72@gmail.com>
2025-01-02 17:32:05 +0530

    Updated UI and added databse manager and patient details model

---
+++ LoginScreen/Models/PatientDetails.swift
@@ -0,0 +1,21 @@
+//
+//  PatientDetails.swift
+//  LoginScreen
+//
+//  Created by Karpahalakshmi on 02/01/25.
+//
+
+import Foundation
+
+struct PatientDetails {
+    let type: String = "patient"
+    var email: String?
+    var mrn: String?
+    var firstName: String?
+    var middleName: String?
+    var lastName: String?
+    var gender: String?
+    var dateOfBirth: Date?
+    var admissionNo: String?
+    var admitDate: Date?
+}
